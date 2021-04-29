b5955515073fd6fb213b345ed19a00835a586d02
Siphesihle1 <percysiphe@gmail.com>
2021-04-28 20:44:51 -0400

    Migrated from sqlite to PostgreSQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3f*v1r=bl#ufa_c-k7v*$3u@914v055^9!!@sk#93l=iiadpi(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['148.100.79.67']
 
 
 # Application definition
@@ -76,11 +76,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'ai_shopping_system',
+        'USER' : 'dbadmin',
+        'PASSWORD' : 'admin12345',
+        'HOST' : 'localhost',
+        'PORT' : '',
+    }    
+        
 }
 
 
