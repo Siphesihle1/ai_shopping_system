aa2e9d6d6f78549ea61cb79fb274fb0e46e383f6
Byron Kruger <2090048@students.wits.ac.za>
2021-05-10 09:53:40 +0200

    main version updated with product modals

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.name
